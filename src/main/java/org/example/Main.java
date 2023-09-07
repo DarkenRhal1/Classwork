3b4f37aecc47da3eb4ef47a70d1b140608754157
Yaw <baffourampadu06@gmail.com>
2023-09-07 13:23:32 -0400

    Add the name variable

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello World!");
-        
+        String name= "Yaw B. AMpadu";
+        System.out.println(name);
+
     }
 }
